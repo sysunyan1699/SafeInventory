9784b043cafa9c7a5e778ef8b023f95fb519d2f1
sunyan <sysunyan1699@gmail.com>
2024-11-22 16:36:43 +0800

    distribute remove useless code

--- distributedransaction-safeinventory/src/test/java/com/example/safeinventory/mapper/InventoryReservationLogMapperTest.java
+++ distributedransaction-safeinventory/src/test/java/com/example/safeinventory/mapper/InventoryReservationLogMapperTest.java
@@ -2,8 +2,6 @@ package com.example.safeinventory.mapper;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class InventoryReservationLogMapperTest {
 
     @Test
--- distributedransaction-safeinventory/src/test/java/com/example/safeinventory/service/RedisDistributedLockTest.java
+++ distributedransaction-safeinventory/src/test/java/com/example/safeinventory/service/RedisDistributedLockTest.java
@@ -1,11 +1,10 @@
 package com.example.safeinventory.service;
 
-import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
